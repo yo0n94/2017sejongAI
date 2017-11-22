8d2e9b12ebdc5740d50f4d60cfaff485a7847d25
yo0n94 <33116833+yo0n94@users.noreply.github.com>
2017-11-22 22:24:55 +0900

    Add files via upload

---
+++ Homework/가설4 약어,감탄사,비속어 등.py
@@ -0,0 +1,54 @@
+from nltk.corpus import movie_reviews
+from nltk.classify import NaiveBayesClassifier
+from nltk.classify.util import accuracy as nltk_accuracy
+
+def extract_features(words):
+    return dict([(word, True) for word in words])
+
+
+if __name__=='__main__':
+    fileids_pos = movie_reviews.fileids('pos')
+    fileids_neg = movie_reviews.fileids('neg')
+
+    features_pos = [(extract_features(movie_reviews.words(
+        fileids=[f])), 'Positive') for f in fileids_pos]
+    features_neg = [(extract_features(movie_reviews.words(
+        fileids=[f])), 'Negative') for f in fileids_neg]
+
+threshold = 0.8
+num_pos = int(threshold * len(features_pos))
+num_neg = int(threshold * len(features_neg))
+
+features_train = features_pos[:num_pos] + features_neg[:num_neg]
+features_test = features_pos[num_pos:] + features_neg[num_neg:]
+
+print('\nNumber of training datapoints:', len(features_train))
+print('Number of test datapoints:', len(features_test))
+
+classifier = NaiveBayesClassifier.train(features_train)
+print('\nAccuracy of the classifier:', nltk_accuracy(classifier, features_test))
+
+N = 15
+print('\nTop ' + str(N) + ' most informative words:')
+for i, item in enumerate(classifier.most_informative_features()):
+    print(str(i+1) + '. ' + item[0])
+    if i == N - 1:
+        break
+
+input_reviews = [
+    "This movie was so unbelievable and stupid. Unromantic, boring, terribly acted... The list goes on and on. Although I can't fault the blame totally on Love. None of the actors were convincing at all, except maybe Tom Wilkenson, although I do think slightly less of him for taking part in this horrible film.",
+    "'WTF!' this movie was so unbelievable and stupid. OMG Unromantic, boring, terribly acted... The list goes on and on. Although I can't fault the blame totally on Love. LOL, None of the actors were convincing at all, except maybe Tom Wilkenson, although I do think slightly less of him for taking part in this Damn horrible film. Yuck!",
+    "Now here's the romance of the decade. Heart-warming doesn't even come close to describing 'If Only' It's a story about love in its purest form. About realizing that any moment someone you care for can be taken away and you have the be certain you did everything to make your life meaningful with that someone. If you're looking for a movie that will make you smile, make you think, make you cry and leave you touched. If Only is the way to go.",
+    "WOW!! Now here's the romance of the decade. Heart-warming doesn't even come close to describing 'If Only' It's a story about love in its purest form. OH, about realizing that any moment someone you care for can be taken away and you have the be certain you did everything to make your life meaningful with that someone. If you're looking for a movie that will make you smile, make you think, make you cry and leave you touched. If Only is the way to go. Hooray!"
+    ]
+
+print("\nMovie review predictions:")
+for review in input_reviews:
+    print("\nReview:", review)
+
+    probabilities = classifier.prob_classify(extract_features(review.split()))
+
+    predicted_sentiment = probabilities.max()
+
+    print("Predicted sentiment:", predicted_sentiment)
+    print("Probability:", round(probabilities.prob(predicted_sentiment), 2))
